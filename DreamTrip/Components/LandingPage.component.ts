41f62d480fbbb1c9a00434e3e0e803cf4bbf31d2
rmendes <rmendes@mypartner.pt>
2024-01-30 15:00:39 +0000

    lari lari

--- DreamTrip/Components/LandingPage.component.ts
+++ DreamTrip/Components/LandingPage.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
-  templateUrl: 'C:\Users\rmendes\Desktop\DreamTrip\DreamTrip\Pages\LandingPage\LandingPage.html',
-  styleUrls: ['C:\Users\rmendes\Desktop\DreamTrip\DreamTrip\Pages\LandingPage\LandingPage.css']
+  templateUrl: '.\LandingPage\LandingPage.html',
+  styleUrls: ['.\LandingPage\LandingPage.css']
 })
 export class LandingPageComponent implements OnInit {
   ngOnInit() {
